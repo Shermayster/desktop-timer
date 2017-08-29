d18c3a39cfb6c589238f235b4669a2c37ad237a7
pavelshermayster <sherm84@gmail.com>
2017-08-29 22:11:30 +0300

    styling update

--- src/app/components/setTimer/setTimer.component.ts
+++ src/app/components/setTimer/setTimer.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-set-timer',
-  templateUrl: './setTimer.component.html'
+  templateUrl: './setTimer.component.html',
+  styleUrls: ['./setTimer.component.scss']
 })
 
 export class SetTimerComponent {
